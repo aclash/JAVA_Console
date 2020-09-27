bdb47bd7dd25e7889151ebc7c8ee1f594276a545
aclash <aclash@163.com>
2020-09-26 22:44:29 -0700

    aclash sub 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -155,6 +155,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        //git aclash sub 1
 //        try {
 //            double[][] matrix = readFile();
 //            int row = matrix.length;
